cd498b61e538ca6a5d74f3d6615e104088ee35dc
brian <briancorrea@gmail.com>
2023-05-31 12:11:49 -0400

    use llm

--- server/services/openai.js
+++ server/services/openai.js
@@ -1,17 +1,21 @@
 import { ConversationChain } from 'langchain/chains';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate } from 'langchain/prompts';
+import { PromptTemplate } from 'langchain/prompts';
 import { ConversationSummaryMemory } from 'langchain/memory';
 
 class OpenAiService {
   constructor () {
-    this.model = new ChatOpenAI({ temperature: 0, verbose: true });
+    this.model = new OpenAI({ temperature: 0, verbose: true });
 
-    this.chatPrompt = ChatPromptTemplate.fromPromptMessages([
-      SystemMessagePromptTemplate.fromTemplate('The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.'),
-      new MessagesPlaceholder('history'),
-      HumanMessagePromptTemplate.fromTemplate('{input}'),
-    ]);
+    this.prompt = PromptTemplate.fromTemplate(`The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+    Relevant pieces of previous conversation:
+    {history}
+
+    (You do not need to use these pieces of information if not relevant)
+
+    Current conversation:
+    Human: {input}
+    AI:`);
 
     this.memory = new ConversationSummaryMemory({ llm: this.model, returnMessages: true });
   }
@@ -19,7 +23,7 @@ class OpenAiService {
   assembleChain () {
     const chain = new ConversationChain({
       memory: this.memory,
-      prompt: this.chatPrompt,
+      prompt: this.prompt,
       llm: this.model,
     });
     return chain;
